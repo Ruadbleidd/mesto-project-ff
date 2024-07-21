4d10368efd54a30617b68ef39a130e38157282a4
Anatoly Polyakov <tolyik.polyakov@yandex.ru>
2024-07-21 16:08:05 +0300

    omg

--- scripts/index.js
+++ scripts/index.js
@@ -8,34 +8,27 @@
 
 // @todo: Вывести карточки на страницу
 
-function createCard(name, link, alt, removeCard) {
+function createCard(name, link, removeCard) {
   const cardTemplate = document.querySelector("#card-template").content;
   const cardContent = cardTemplate.querySelector(".card").cloneNode(true);
 
   cardContent.querySelector(".card__title").textContent = name;
   cardContent.querySelector(".card__image").src = link;
-  cardContent.querySelector(".card__image").alt = alt;
-
+  removeCard = DeleteCard;
   const deleteButton = cardContent.querySelector(".card__delete-button");
   deleteButton.addEventListener("click", removeCard);
   return cardContent;
-
-  function removeCard(evt) {
-    const evtTarget = evt.target;
-    const card = evtTarget.closest(".card");
-    card.remove();
-  }
 }
 
 const placeList = document.querySelector(".places__list");
 
-initialCards.forEach(function (item) {
-  const cardContent = createCard(item.link, item.name);
+initialCards.forEach(function (card) {
+  const cardContent = createCard(card.link, card.name);
   placeList.append(cardContent);
 });
 
-// function removeCard(evt) {
-//   const evtTarget = evt.target;
-//   const card = evtTarget.closest(".card");
-//   card.remove();
-// }
+function DeleteCard(evt) {
+  const evtTarget = evt.target;
+  const card = evtTarget.closest(".card");
+  card.remove();
+}
